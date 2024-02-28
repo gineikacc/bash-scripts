4967c6d75a8d85f638a7526a7c10992bd0092e0b
gineikacc <airolen11@gmail.com>
2024-02-28 19:00:51 +0200

    fixed $ "roll 0" crash bug

--- roll.sh
+++ roll.sh
@@ -1,19 +1,19 @@
 #!/bin/env bash
 function roll() {
-roll_size=100
-if [[ -n $1 ]] && [[ $1 -eq $1 ]] && [[ $1 -ge 0 ]]; then
-	roll_size=$1
-fi
-if [[ $roll_size -eq 1 ]]; then
-	result=1
-else
-	result=$(( $RANDOM%roll_size + 1))
-fi
+	roll_size=100
+	COLOR_YELLOW='\e[0;33m'
+	COLOR_RESET='\e[0m'
 
-YELLOW='\e[0;33m'
-NC='\e[0m'
+	if [[ -n $1 ]] && [[ $1 -eq $1 ]] && [[ $1 -ge 1 ]]; then
+		roll_size=$1
+	fi
+	if [[ $roll_size -eq 1 ]]; then
+		result=1
+	else
+		result=$(( $RANDOM%roll_size + 1 ))
+	fi
 
-echo -e "${YELLOW}$(whoami) rolls $result (1-${roll_size})${NC}"
+	echo -e "${COLOR_YELLOW}$(whoami) rolls $result (1-${roll_size})${COLOR_RESET}"
 }
 
 export -f roll
