ec5dddb7701fc5d200d9f4622929d8bf0be8305a
gineikacc <airolen11@gmail.com>
2024-02-28 19:15:30 +0200

    Fixed roll 0 bug and added error messages

--- roll.sh
+++ roll.sh
@@ -2,10 +2,15 @@
 function roll() {
 	roll_size=100
 	COLOR_YELLOW='\e[0;33m'
+	COLOR_RED='\e[0;31m'
 	COLOR_RESET='\e[0m'
 
 	if [[ -n $1 ]] && [[ $1 -eq $1 ]] && [[ $1 -ge 1 ]]; then
 		roll_size=$1
+	fi
+	if [[ -n $1 ]] && [[ $1 -le 0 ]]; then
+		echo -e "${COLOR_RED}Can't have numbers less than 1!$COLOR_RESET"
+		return 1
 	fi
 	if [[ $roll_size -eq 1 ]]; then
 		result=1
