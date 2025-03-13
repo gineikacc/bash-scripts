fa8ec3225696e493639d41d9d9b86acba390a337
gineikacc <airolen11@gmail.com>
2025-03-13 23:27:53 +0200

    NEW: streamlined bash-script loading architecture

---
+++ main.sh
@@ -0,0 +1,6 @@
+#!/bin/env bash
+source ~/.config/bash-scripts/b.sh
+source ~/.config/bash-scripts/roll.sh
+source ~/.config/bash-scripts/light.sh
+source ~/.config/bash-scripts/palette.sh
+source ~/.config/bash-scripts/rape.sh
