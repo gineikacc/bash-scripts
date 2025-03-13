0cb131547ae5fd25152794a79a4ae630c36f934f
gineikacc <airolen11@gmail.com>
2025-03-13 23:26:44 +0200

    NEW: online gallery sc(rape) tech

---
+++ rape.sh
@@ -0,0 +1,24 @@
+#!/bin/env bash
+function rapego() {
+	mkdir -p ~/temp/img_scrape/
+	cd ~/temp/img_scrape/
+	gallery-dl --destination . $1
+	mkdir -p ~/temp/img_scrape/.flat/
+	find . -type f -iname "*.*" | grep -v '.flat' | xargs -0 -I {} mv {} ~/temp/img_scrape/.flat/
+	return
+}
+function rapecheck() {
+	qimgv ~/temp/img_scrape/.flat/
+}
+function rapesave() {
+	cp ~/temp/img_scrape/.flat/* ~/img/
+	return
+}
+function rapepurge() {
+	rm -rf ~/temp/img_scrape/
+	return
+}
+export -f rapego
+export -f rapecheck
+export -f rapesave
+export -f rapepurge
